a65dda804adb4754e3f88ac6961f916072994aa7
Gambhir Dhruv <75164918+dhruv-gambhir@users.noreply.github.com>
2023-02-23 02:56:27 +0800

    general updates

--- build/rover_gui/CTestTestfile.cmake
+++ build/rover_gui/CTestTestfile.cmake
@@ -1,9 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/dhruv/catkin_ws/src
-# Build directory: /home/dhruv/catkin_ws/build
+# Source directory: /home/dhruv/catkin_ws/src/rover_gui
+# Build directory: /home/dhruv/catkin_ws/build/rover_gui
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("controller")
-subdirs("rover_gui")
